1df4e523ad0b8e879b26d3a2757451fc5a645e8c
Dylan Mendez <Dylan_702@hotmail.fr>
2018-11-23 14:04:05 +0100

    test

---
+++ app/src/main/java/com/example/dmendez/projet_android_dylan_julian/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.example.dmendez.projet_android_dylan_julian.ui.main;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
