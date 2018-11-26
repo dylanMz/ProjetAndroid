cd6ed735e3d2d6d1728a6e3b2711ba4289c1ce7e
Julian DEMOIS <julian.demois@hotmail.com>
2018-11-26 13:19:03 +0100

    PushPushPushhhhhhh

--- app/src/main/java/com/example/dmendez/projet_android_dylan_julian/AccueilActivity.java
+++ app/src/main/java/com/example/dmendez/projet_android_dylan_julian/AccueilActivity.java
@@ -1,13 +1,40 @@
 package com.example.dmendez.projet_android_dylan_julian;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class AccueilActivity extends AppCompatActivity {
+public class AccueilActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView imageJeu1;
+    private ImageView imageJeu2;
+    private EditText editTextPrenom;
+    private String lePrenom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+
+        imageJeu1 = (ImageView) this.findViewById(R.id.imageView_Jeu1);
+        imageJeu2 = (ImageView) this.findViewById(R.id.imageView_Jeu2);
+        editTextPrenom = (EditText) this.findViewById(R.id.editText_prenom);
+
+        imageJeu1.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        //Intent unIntent = new Intent(this, MainActivity.class   );
+        //lePrenom = editTextPrenom.getText().toString();
+        //unIntent.putExtra("Joueur", lePrenom);
+        //this.startActivity(unIntent);
+
+
     }
 }
