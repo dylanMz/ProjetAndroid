ea215e87d749ff3934d29b0031bad6997f33f92e
Julian DEMOIS <julian.demois@hotmail.com>
2019-01-09 22:03:33 +0100

    ajout score encorez

--- app/src/main/java/com/example/dmendez/projet_android_dylan_julian/ScoreActivity.java
+++ app/src/main/java/com/example/dmendez/projet_android_dylan_julian/ScoreActivity.java
@@ -1,13 +1,47 @@
 package com.example.dmendez.projet_android_dylan_julian;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ScoreActivity extends AppCompatActivity {
+    private Button btnAccueil;
+    private Button btnRetour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+
+        //Initilisation des controles android studio
+        btnAccueil = (Button) this.findViewById(R.id.button_accueil);
+        btnRetour = (Button) this.findViewById(R.id.button_retour);
+
+        btnAccueil.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent Intent1 = new Intent(ScoreActivity.this, AccueilActivity.class);
+                startActivity(Intent1);
+
+            }
+        });
+
+        btnRetour.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent Intent1 = new Intent(ScoreActivity.this, MainActivity.class);
+                startActivity(Intent1);
+
+            }
+        });
+
+
+
+
     }
+
 }
