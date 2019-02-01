691909c4538f3270de630aa24806566ed63abe92
Julian DEMOIS <julian.demois@hotmail.com>
2019-02-01 16:09:06 +0100

    PUSH SCORE ACTIVITES

--- app/src/main/java/com/example/dmendez/projet_android_dylan_julian/ScoreActivity.java
+++ app/src/main/java/com/example/dmendez/projet_android_dylan_julian/ScoreActivity.java
@@ -2,35 +2,48 @@ package com.example.dmendez.projet_android_dylan_julian;
 
 import android.app.AlertDialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class ScoreActivity extends AppCompatActivity {
+
     private Button btnAccueil;
     private Button btnRetour;
+    private TextView testJson;
 
     private ListView listeFacile;
     private ListView listeMoyen;
     private ListView listeDifficile;
     private ListView listeExtreme;
-    private Adapter unadapter;
+
     private gestionBdd db =new gestionBdd(this);
     private ArrayList<Score> ensScore = new ArrayList<>();
     private Score unScore;
     private String lePrenom;
     private ImageView imgInfoScore;
-
-
 
     private static final String NOM_BDD2 ="scoreBD";
     private static final String id2 = "scoreId";
@@ -39,24 +52,25 @@ public class ScoreActivity extends AppCompatActivity {
     private static final String niveau = "scoreNiveau";
 
     private gestionBdd bdd;
-        private ArrayList<String> IdScoreF = new ArrayList<String>();
-        private ArrayList<String> NameF = new ArrayList<String>();
-        private ArrayList<String> ScoreF = new ArrayList<String>();
+    private ArrayList<String> IdScoreF = new ArrayList<String>();
+    private ArrayList<String> NameF = new ArrayList<String>();
+    private ArrayList<String> ScoreF = new ArrayList<String>();
 
-        private ArrayList<String> IdScoreM = new ArrayList<String>();
-        private ArrayList<String> NameM = new ArrayList<String>();
-        private ArrayList<String> ScoreM = new ArrayList<String>();
+    private ArrayList<String> IdScoreM = new ArrayList<String>();
+    private ArrayList<String> NameM = new ArrayList<String>();
+    private ArrayList<String> ScoreM = new ArrayList<String>();
 
-        private ArrayList<String> IdScoreD = new ArrayList<String>();
-        private ArrayList<String> NameD = new ArrayList<String>();
-        private ArrayList<String> ScoreD = new ArrayList<String>();
+    private ArrayList<String> IdScoreD = new ArrayList<String>();
+    private ArrayList<String> NameD = new ArrayList<String>();
+    private ArrayList<String> ScoreD = new ArrayList<String>();
 
-        private ArrayList<String> IdScoreE = new ArrayList<String>();
-        private ArrayList<String> NameE = new ArrayList<String>();
-        private ArrayList<String> ScoreE = new ArrayList<String>();
+    private ArrayList<String> IdScoreE = new ArrayList<String>();
+    private ArrayList<String> NameE = new ArrayList<String>();
+    private ArrayList<String> ScoreE = new ArrayList<String>();
 
-
-
+    String uneUrl = String.format("http://10.0.2.2/TestPhp/bdd_recuperation_score.php");
+    private String TAG = MainActivity.class.getSimpleName();
+    private ArrayList<HashMap<String, String>> List_score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,28 +80,26 @@ public class ScoreActivity extends AppCompatActivity {
         //Initilisation des controles android studio
         btnAccueil = (Button) this.findViewById(R.id.button_accueil);
         btnRetour = (Button) this.findViewById(R.id.button_retour);
-
         imgInfoScore = (ImageView) this.findViewById(R.id.imageView_infoScore);
-
         listeFacile = (ListView) this.findViewById(R.id.listview_score_facile);
         listeMoyen = (ListView) this.findViewById(R.id.listview_score_moyen);
         listeDifficile = (ListView) this.findViewById(R.id.listview_score_difficile);
         listeExtreme = (ListView) this.findViewById(R.id.listview_score_extreme);
 
-        //bordure1 = (ImageView) this.findViewById(R.id.
-
         bdd = new gestionBdd(this);
 
+        //Récupère le nom du joueur
         lePrenom = this.getIntent().getExtras().getString("Joueur");
 
+        List_score = new ArrayList<>();
+        new GetScore().execute();
 
-        getLesScoresFacile();
-        getLesScoresMoyen();
-        getLesScoresDifficile();
-        getLesScoreExtreme();
+        //getLesScoresFacile();
+        //getLesScoresMoyen();
+        //getLesScoresDifficile();
+        //getLesScoreExtreme();
 
-        //unadapter = new Adapter(getApplicationContext(), unScore.getNomJoueur() , );
-
+        //Permet de retourner a l'accueil
         btnAccueil.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v)
@@ -98,6 +110,7 @@ public class ScoreActivity extends AppCompatActivity {
             }
         });
 
+        //Permet de retourner sur le jeu
         btnRetour.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v)
@@ -109,34 +122,144 @@ public class ScoreActivity extends AppCompatActivity {
             }
         });
 
+        //Permet d'afficher comment sont calculer les scores
         imgInfoScore.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v)
             {
-                // setup the alert builder
+
                 AlertDialog.Builder builder = new AlertDialog.Builder(ScoreActivity.this );
                 builder.setTitle("Scores");
                 builder.setMessage("Les scores sont calculés en fonction du nombre de personnages trouvés, si vous avez gagné, le temps restant non utilisé vous rapportent aussi des points.");
 
-                // add a button
+                // ajoute un bouton
                 builder.setPositiveButton("OK", null);
 
-                // create and show the alert dialog
+                // Créer et affiche la boite de dialogue
                 AlertDialog dialog = builder.create();
                 dialog.show();
+
 
             }
         });
 
 
+    }
+
+    private class GetScore extends AsyncTask<Void, Void, Void> {
+
+        protected Void doInBackground(Void... arg0)
+        {
+            HttpHandler sh = new HttpHandler();
+            // déclenche une requête sur l'url
+            String url = uneUrl;
+            String jsonStr = sh.makeServiceCall(url);
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+                try {
+                    JSONArray contacts = new JSONArray(jsonStr);
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject c = contacts.getJSONObject(i);
+                        String nom = c.getString("nom");
+                        String score = c.getString("score");
+                        String niveau = c.getString("niveau");
+                        HashMap<String, String> unScore  = new HashMap<>();
+                        unScore.put("nom", nom);
+                        unScore.put("score", score);
+                        unScore.put("niveau", niveau);
+                        List_score.add(unScore);
+
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "erreur de Parse Json: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext()," erreur de Parse Json:" + e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+
+            } else {
+                Log.e(TAG, "Pb récup Json sur le serveur.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext()," Pb récup Json sur le serveur. Voir Logcat!", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            //Recupère la taille de la liste
+            int tailleListeFacile;
+            tailleListeFacile = List_score.size() - 1;
+
+            //Tant qu'il y a des données dans la liste ajoute les scores et noms des joueurs dans les bonnes listes
+            while (tailleListeFacile >= 0)
+            {
+                if(List_score.get(tailleListeFacile).get("niveau").equals("Facile"))
+                {
+                    NameF.add(List_score.get(tailleListeFacile).get("nom"));
+                    ScoreF.add(List_score.get(tailleListeFacile).get("score"));
+                }
+
+                if(List_score.get(tailleListeFacile).get("niveau").equals("Moyen"))
+                {
+                    NameM.add(List_score.get(tailleListeFacile).get("nom"));
+                    ScoreM.add(List_score.get(tailleListeFacile).get("score"));
+                }
+
+                if(List_score.get(tailleListeFacile).get("niveau").equals("Difficile"))
+                {
+                    NameD.add(List_score.get(tailleListeFacile).get("nom"));
+                    ScoreD.add(List_score.get(tailleListeFacile).get("score"));
+                }
+
+                if(List_score.get(tailleListeFacile).get("niveau").equals("Extreme"))
+                {
+                    NameE.add(List_score.get(tailleListeFacile).get("nom"));
+                    ScoreE.add(List_score.get(tailleListeFacile).get("score"));
+                }
+
+                tailleListeFacile=tailleListeFacile-1;
+            }
 
 
+            //Les adapters prennent en paramètres les listes de scores et noms qui leurs correspondents
+            Adapter unAdapterFacile = new Adapter(ScoreActivity.this, NameF,ScoreF);
+            Adapter unAdapterMoyen = new Adapter(ScoreActivity.this, NameM,ScoreM);
+            Adapter unAdapterDifficile = new Adapter(ScoreActivity.this, NameD,ScoreD);
+            Adapter unAdapterExtreme = new Adapter(ScoreActivity.this, NameE,ScoreE);
 
+            //Affiche dans les listview le contenu.
+            listeFacile.setAdapter(unAdapterFacile);
+            listeMoyen.setAdapter(unAdapterMoyen);
+            listeDifficile.setAdapter(unAdapterDifficile);
+            listeExtreme.setAdapter(unAdapterExtreme);
+
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(ScoreActivity.this, "progression en cours... ", Toast.LENGTH_LONG).show();
+
+
+        }
 
     }
 
     //Retourne l'ensemble des scores réalisés en mode facile
-    public void getLesScoresFacile(){
+    /*public void getLesScoresFacile(){
         String reqSelect = "SELECT scoreJoueur, scoreNom FROM " + NOM_BDD2+ " WHERE "+niveau + " LIKE 'Facile' ORDER BY " +score + " DESC LIMIT 9";
 
         SQLiteDatabase db = bdd.getReadableDatabase();
@@ -220,6 +343,6 @@ public class ScoreActivity extends AppCompatActivity {
         listeExtreme.setAdapter(ad);
         unCurseur.close();
     }
-
+*/
 }
 
